e82bac94ec44d0b6af713e967a13eb6be69378ed
Alexis PIERRE DIT LAMBERT <a.pierreditlambert@gmail.com>
2022-11-14 20:44:22 +0000

    MAJ vision node

--- jetsonNano/ros2_ws/src/vision/vision/vision.py
+++ jetsonNano/ros2_ws/src/vision/vision/vision.py
@@ -16,6 +16,40 @@ import imutils
 from webcolors import rgb_to_name,CSS3_HEX_TO_NAMES,hex_to_rgb #python3 -m pip install webcolors
 from scipy.spatial import KDTree
 
+class ShapeDetector:
+    def __init__(self):
+        pass
+    def detect(self, c):
+        # initialize the shape name and approximate the contour
+        shape = "unidentified"
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+
+        # if the shape is a triangle, it will have 3 vertices
+        if len(approx) == 3:
+            shape = "triangle"
+        # if the shape has 4 vertices, it is either a square or
+        # a rectangle
+        elif len(approx) == 4:
+            # compute the bounding box of the contour and use the
+            # bounding box to compute the aspect ratio
+            (x, y, w, h) = cv2.boundingRect(approx)
+            ar = w / float(h)
+            # a square will have an aspect ratio that is approximately
+            # equal to one, otherwise, the shape is a rectangle
+            shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
+        # if the shape is a pentagon, it will have 5 vertices
+        elif len(approx) == 5:
+            shape = "pentagon"
+        elif len(approx) == 6:
+            shape = "hexagon"
+        elif len(approx) == 10 or len(approx) == 12:
+            shape = "star"
+        # otherwise, we assume the shape is a circle
+        else:
+            shape = "circle"
+        # return the name of the shape
+        return shape    
 
 class Vision(Node):
     """
@@ -53,22 +87,42 @@ class Vision(Node):
         
         img = self.bridge.imgmsg_to_cv2(imgMsg, desired_encoding='passthrough')
         
+        # Mask of purple color
+        image = cv2.inRange(img, (29, 117, 244), (29, 244, 236))
         
-        lower_red = np.array([0, 0, 50], dtype = "uint8") 
+        ## Image processing 
+        resized = imutils.resize(image, width=300) #redimensionnement
+        ratio = image.shape[0] / float(resized.shape[0])
+        
+        # convert the resized image to blur it slightly,
+        # and threshold it
 
-        upper_red= np.array([50, 50, 255], dtype = "uint8")
+        blurred = cv2.GaussianBlur(image, (5, 5), 0)
+        thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+        
+        # find contours in the thresholded image and initialize the
+        # shape detector
+        # get all the possible shapes
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE)
+        
 
-        mask = cv2.inRange(img, lower_red, upper_red)
+        cnts = imutils.grab_contours(cnts)
+        sd = ShapeDetector()
 
-        detect = np.mean(mask)
-
-        if detect > 0:
-            hookDetected = True
-            print("Hook found")
+        # loop over the contours
+        for c in cnts: 
+            #detect shape from contour
+            shape = sd.detect(c)
             
-        else:
-            hookDetected = False
-            print("None Hook")
+            if (shape == "circle"): 
+                hookDetected = True
+                self.get_logger().info('Hook detected')
+            else: hookDetected = False
+            # resize the contour
+            c = c.astype("float")
+            c *= ratio
+            c = c.astype("int")
 
 
         # Publication on the /hook topic
