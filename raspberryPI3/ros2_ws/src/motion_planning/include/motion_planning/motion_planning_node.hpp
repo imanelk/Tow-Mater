2bea9f7df524e51476ae8dc878b566c41e7c27df
Alexis PIERRE DIT LAMBERT <a.pierreditlambert@gmail.com>
2022-11-23 23:20:16 +0100

    motion_planning_v2 : from state machine

--- raspberryPI3/ros2_ws/src/motion_planning/include/motion_planning/motion_planning_node.hpp
+++ raspberryPI3/ros2_ws/src/motion_planning/include/motion_planning/motion_planning_node.hpp
@@ -5,28 +5,32 @@
 #include <string.h>  
 
 #define PERIOD_UPDATE_MOTION 5ms    // Update period of the movement calculation
-#define PERIOD_CHECK_SECURITY 10ms // Update period of the propulsion and steering commands in [ms]
 
-#define INITIAL_VELOCITY 0
-#define INITIAL_STEER 0
-
-#define MAX_VELOCITY 1.2   // [m/s]
+#define INITIAL_VELOCITY 0.0
+#define INITIAL_STEER 0.0
 
 // ---- Final reverse and towing ----
 
-#define FINAL_REVERSE_VELOCITY -0.8 // [m/s]
+#define REVERSE_VELOCITY -1.0
+#define FINAL_REVERSE_VELOCITY -0.7 // [m/s]
 
-#define LOCK_DISTANCE 21    // Lock distance (between US rear center and the damaged car) [cm]
 #define LOCK_WAITING_TIME 4 //Waiting time after the lock order [s]
 
 #define TOWING_VELOCITY 1.0 // [m/s] 
-#define TOWING_DURATION 10000ms 
+#define TOWING_DISTANCE 5000 // [cm]
 
 // ---- Trajectories
 
 // No U-turn (NUT)
 
 #define NB_NUT_POINTS 3
+
+
+
+// Security
+#define LLS_DISTANCE 15 //LowLevelSecurityDistance = 20cm (LLS_DISTANCE)
+
+
  
 
 #endif /*__motion_planning_node_HPP */
