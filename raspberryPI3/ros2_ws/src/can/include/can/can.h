35be72f969430a4aa6e1b321db9872fe43be2188
Alexis PIERRE DIT LAMBERT <a.pierreditlambert@gmail.com>
2022-11-24 21:28:27 +0100

    Update hook status (true=lock / false=unlock)

--- raspberryPI3/ros2_ws/src/can/include/can/can.h
+++ raspberryPI3/ros2_ws/src/can/include/can/can.h
@@ -21,8 +21,8 @@
 
 #define ID_HOOK 0x111
 
-#define HOOK_UNLOCK 0x0     //frame[0]
-#define HOOK_LOCK 0x1       //frame[0]
+#define HOOK_LOCK 0x0     //frame[0]
+#define HOOK_UNLOCK 0x1       //frame[0]
 
 #define CALIBRATION_REQUEST	0x1		//frame[0]
 #define CALIBRATION_IN_PROGRESS 0x2	//frame[0]
