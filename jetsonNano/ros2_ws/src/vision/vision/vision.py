a06161d493a4508fced4b34059f7c6aa796a9e3c
Léonie Hatte <hatte@insa-toulouse.fr>
2022-11-24 13:17:44 +0100

    update vision

--- jetsonNano/ros2_ws/src/vision/vision/vision.py
+++ jetsonNano/ros2_ws/src/vision/vision/vision.py
@@ -61,7 +61,7 @@ class Vision(Node):
             x_coordinates = (box_coordinates[0][0][0]) + (((box_coordinates[0][1][0])-(box_coordinates[0][0][0]))/2)
             y_coordinates = (box_coordinates[0][0][1]) + (((box_coordinates[0][2][1])-(box_coordinates[0][0][1]))/2)
             middle_coordinates = [x_coordinates, y_coordinates]
-            width_qr = (box_coordinates[0][1][0])-(box_coordinates[0][0][0])
+            width_qr = box_coordinates[0][1][0]-box_coordinates[0][0][0]
         else:
             x_coordinates = -1.0
             y_coordinates = -1.0
