2a7c44a58db23947c02a1b01c4d898cc95d18440
Alexis PIERRE DIT LAMBERT <a.pierreditlambert@gmail.com>
2023-01-06 11:38:05 +0000

    update

--- raspberryPI3/ros2_ws/src/obstacle_detection/include/obstacle_detection/obstacle_detection_node.h
+++ raspberryPI3/ros2_ws/src/obstacle_detection/include/obstacle_detection/obstacle_detection_node.h
@@ -5,6 +5,7 @@
 #include <string.h>  
 
 
-#define OBSTACLE_PRESENT 50 //Threshold distance which the object detected by the sensor is considered as obstacle
+#define OBSTACLE_PRESENT 75 //Threshold distance which the object detected by the sensor is considered as obstacle
+#define PERIOD_UPDATED_COUNTER_OBSTACLE 1s //Time for an fixed obstacle identification
 
 #endif /*__obstacle_detection_node_H*/
