73a735702910a7f4ffbaa005d3ff6f06041cfa79
Lmendesinsa <mendeslaura.66000@gmail.com>
2023-01-12 11:52:10 +0100

    Initial commit

---
+++ jetsonNano/ros2_ws/src/side_detection/include/side_detection_node.h
@@ -0,0 +1,11 @@
+#ifndef __side_detection_node_H
+#define __side_detection_node_H
+
+#include <stdint.h>
+#include <string.h>  
+
+#define PERIOD_UPDATE_CMD 1ms //Period to update proupulsion and steering command in [ms]
+
+
+
+#endif /*__ side_detection_node_H */
---
+++ jetsonNano/ros2_ws/src/side_detection/src/side_detection_node.cpp
@@ -0,0 +1,61 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+
+#include "interfaces/msg/obstacle_side.hpp"
+#include "../include/side_detection_node.h"
+
+using namespace std;
+using placeholders::_1;
+
+
+class side_detection : public rclcpp::Node {
+
+public:
+    side_detection()
+    : Node("side_detection_node")
+    {
+
+        publisher_side_= this->create_publisher<interfaces::msg::ObstacleSide>("obstacle_side", 10);
+
+        subscription_scan_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+        "scan", 10, std::bind(&obstacle_detector::scanCallback, this, _1));
+
+        RCLCPP_INFO(this->get_logger(), "side_detection_node READY");
+    }
+
+    
+private:
+
+    /* 
+    */
+    void scanDataCallback(const interfaces::msg::Gnss & gnssDc) {
+        
+      
+    }
+
+     
+    // ---- Private variables ----
+
+
+    //Publishers
+    rclcpp::Publisher<interfaces::msg::ObstacleSide>::SharedPtr publisher_side_;
+
+    //Subscribers
+    rclcpp::Subscription<sensor_msgs::LaserScan>::SharedPtr subscription_scan_;
+
+    //Timer
+    rclcpp::TimerBase::SharedPtr timer_;
+
+};
+
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<side_detection>();
+
+  rclcpp::spin(node);
+
+  rclcpp::shutdown();
+  return 0;
+}
