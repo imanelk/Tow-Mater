4fe1551b6088912b9f6e202c64d003fa627c118d
Lmendesinsa <mendeslaura.66000@gmail.com>
2023-01-12 19:58:31 +0100

    Build OK, need to test it

--- jetsonNano/ros2_ws/src/side_detection/include/side_detection_node.h
+++ jetsonNano/ros2_ws/src/side_detection/include/side_detection_node.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <string.h>  
 
-#define PERIOD_UPDATE_CMD 1ms //Period to update proupulsion and steering command in [ms]
+#define PERIOD_UPDATE_CMD 1ms 
 
 
 
--- jetsonNano/ros2_ws/src/side_detection/src/side_detection_node.cpp
+++ jetsonNano/ros2_ws/src/side_detection/src/side_detection_node.cpp
@@ -18,7 +18,7 @@ public:
         publisher_side_= this->create_publisher<interfaces::msg::ObstacleSide>("obstacle_side", 10);
 
         subscription_scan_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
-        "scan", 10, std::bind(&obstacle_detector::scanCallback, this, _1));
+        "scan", 10, std::bind(&side_detection::scanDataCallback, this, _1));
 
         RCLCPP_INFO(this->get_logger(), "side_detection_node READY");
     }
@@ -26,35 +26,57 @@ public:
     
 private:
 
-    /* 
+    /* Update the side where there is a closer obstacle 
+    *
+    *  This function is called when a message is published on the "/scan" topic
     */
     void scanDataCallback(const sensor_msgs::msg::LaserScan & scan) {
+        auto sideMsg = interfaces::msg::ObstacleSide();  
         int size;
         int sum_left = 0;
         int sum_right = 0;
+        float mean_left;
+        float mean_right;
 
+
+        // Get the number of points get with the LiDAR
         size = sizeof scan.intensities/sizeof scan.intensities[0];
         RCLCPP_INFO(this->get_logger(), "La taille du tablea est %d", size);
 
-        for(int i=0; i<= (int)size/2; i++){
+
+        // Get the side where the intensity of the points is the highest
+        for(int i=0; i< (int)(size/2) ; i++){
             sum_left = scan.intensities[i] + sum_left;
         }
-        for(int i=(int)size/2; i<size; i++){
+        for(int i=(int)(size/2); i<size; i++){
             sum_right = scan.intensities[i] + sum_right;
         }
 
-      
+        mean_left = sum_left / (int)(size/2);
+        mean_right = sum_right / (int)(size/2);
+
+        RCLCPP_INFO(this->get_logger(), "Les moyennes sont : %f (Left) et %f (right)", mean_left, mean_right);
+
+        if (mean_left >= mean_right){
+            sideMsg.left_lidar = true;
+            sideMsg.right_lidar = false;
+        }
+        else{
+            sideMsg.left_lidar = false;
+            sideMsg.right_lidar = true;
+        }
+
+        publisher_side_->publish(sideMsg);
     }
 
      
     // ---- Private variables ----
 
-
     //Publishers
     rclcpp::Publisher<interfaces::msg::ObstacleSide>::SharedPtr publisher_side_;
 
     //Subscribers
-    rclcpp::Subscription<sensor_msgs::LaserScan>::SharedPtr subscription_scan_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_scan_;
 
     //Timer
     rclcpp::TimerBase::SharedPtr timer_;
