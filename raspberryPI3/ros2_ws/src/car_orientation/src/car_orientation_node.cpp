a21cfc133c07478cfeb33d54d97b0c58a8602d21
Lmendesinsa <mendeslaura.66000@gmail.com>
2023-01-23 17:12:00 +0100

    Update

--- raspberryPI3/ros2_ws/src/car_orientation/src/car_orientation_node.cpp
+++ raspberryPI3/ros2_ws/src/car_orientation/src/car_orientation_node.cpp
@@ -72,10 +72,11 @@ private:
     * This function is called when a message is published on the "/imu/mag" topic
     * 
     */
-    void magCallback(const sensor_msgs::msg::MagneticField & MAG){  
-        geometry_msgs::msg::Vector3 meas;
-        meas = MAG.magnetic_field;      
-        direction = atan2(meas.y, meas.x) * rad2deg ;
+    void magCallback(const sensor_msgs::msg::MagneticField MAG){  
+
+        update_min_max_dc(MAG.magnetic_field) ;
+        correct_mag_dc(MAG.magnetic_field) ;    
+        direction = atan2(corrected_y, corrected_x) * rad2deg ;
     }
 
     /* Update direction_dc from IMU [callback function]  :
@@ -83,11 +84,92 @@ private:
     * This function is called when a message is published on the "/imu/mag_dc" topic
     * 
     */
-    void magDcCallback(const sensor_msgs::msg::MagneticField & MAG){  
-        geometry_msgs::msg::Vector3 meas = MAG.magnetic_field;      
-        direction_dc = atan2(meas.y, meas.x) * rad2deg ;
+    void magDcCallback(const sensor_msgs::msg::MagneticField MAG){  
+        // geometry_msgs::msg::Vector3 meas = MAG.magnetic_field; 
+        update_min_max_dc(MAG.magnetic_field) ;
+        correct_mag_dc(MAG.magnetic_field) ;
+        direction_dc = atan2(corrected_y_dc, corrected_x_dc) * rad2deg ;
     }
 
+    void update_min_max_dc(const geometry_msgs::msg::Vector3 meas)
+    {
+        if (meas.x < xmin_dc){
+            xmin_dc = meas.x ;
+        }
+        if (meas.x > xmax_dc){
+            xmax_dc = meas.x ;
+        }
+        if (meas.y < ymin_dc){
+            ymin_dc = meas.y ;
+        }
+        if (meas.y > ymax_dc){
+            ymax_dc = meas.y ;
+        }
+        if (meas.z < zmin_dc){
+            zmin_dc = meas.z ;
+        }
+        if (meas.z > zmax_dc){
+            zmax_dc = meas.z ;
+        } 
+    }
+    void correct_mag_dc(const geometry_msgs::msg::Vector3 meas){
+        float offset_x = (xmax_dc + xmin_dc)/2 ;
+        float offset_y = (ymax_dc + ymin_dc)/2 ;
+        
+        float avg_delta_x = (xmax_dc - xmin_dc)/2 ;
+        float avg_delta_y = (ymax_dc - ymin_dc)/2 ;
+        float avg_delta_z = (zmax_dc - zmin_dc)/2 ;
+
+        float avg_delta = (avg_delta_x + avg_delta_y + avg_delta_z)/3 ;
+
+        float scale_x = avg_delta / avg_delta_x ;
+        float scale_y = avg_delta / avg_delta_y ;
+
+
+        corrected_x_dc = (meas.x - offset_x) * scale_x ;
+        corrected_y_dc = (meas.y - offset_y) * scale_y ;
+
+    }
+
+    void update_min_max(const geometry_msgs::msg::Vector3 meas)
+    {
+        if (meas.x < xmin){
+            xmin = meas.x ;
+        }
+        if (meas.x > xmax){
+            xmax = meas.x ;
+        }
+        if (meas.y < ymin){
+            ymin = meas.y ;
+        }
+        if (meas.y > ymax){
+            ymax = meas.y ;
+        }
+        if (meas.z < zmin){
+            zmin = meas.z ;
+        }
+        if (meas.z > zmax){
+            zmax = meas.z ;
+        } 
+    }
+    void correct_mag(const geometry_msgs::msg::Vector3 meas){
+        float offset_x = (xmax + xmin)/2 ;
+        float offset_y = (ymax + ymin)/2 ;
+        
+        float avg_delta_x = (xmax - xmin)/2 ;
+        float avg_delta_y = (ymax - ymin)/2 ;
+        float avg_delta_z = (zmax - zmin)/2 ;
+
+        float avg_delta = (avg_delta_x + avg_delta_y + avg_delta_z)/3 ;
+
+        float scale_x = avg_delta / avg_delta_x ;
+        float scale_y = avg_delta / avg_delta_y ;
+
+
+        corrected_x = (meas.x - offset_x) * scale_x ;
+        corrected_y = (meas.y - offset_y) * scale_y ;
+
+    }
 
     /* 
     *
@@ -129,6 +211,26 @@ private:
     // Constants
     float pi = 3.14159265359 ;
     float rad2deg = 180 / pi;
+
+    float xmin = 100 ;
+    float xmax = 0 ;
+    float ymin = 100 ;
+    float ymax = 0 ;
+    float zmin = 100 ;
+    float zmax = 0 ;
+
+    float corrected_x ;
+    float corrected_y ;
+
+    float xmin_dc = 100 ;
+    float xmax_dc = 0 ;
+    float ymin_dc = 100 ;
+    float ymax_dc = 0 ;
+    float zmin_dc = 100 ;
+    float zmax_dc = 0 ;
+
+    float corrected_x_dc ;
+    float corrected_y_dc ;
 
 
     //Publisher
